82089bd1a598c3a94576cd32e695a6f0fc1758ba
T1t0-G40n4 <titogaona84@gmail.com>
2025-04-25 20:26:27 +0000

    Proyecto toDoList T1t0-G40n4

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState(""); // Estado para el valor del input
+	const [tasks, setTasks] = useState([]); // Estado para la lista de tareas
+  
+	// Maneja la tecla "Enter" para agregar una nueva tarea
+	const handleKeyUp = (e) => {
+	  if (e.key === "Enter" && inputValue.trim() !== "") {
+		setTasks([...tasks, inputValue.trim()]);
+		setInputValue(""); // Limpia el input después de agregar la tarea
+	  }
+	};
+  
+	// Elimina una tarea según su índice
+	const removeTask = (indexToRemove) => {
+	  const updatedTasks = tasks.filter((_, index) => index !== indexToRemove);
+	  setTasks(updatedTasks);
+	};
+  
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <>
+		<div className="container">
+		  <span className="title">Tasks to do</span>
+		  <ul>
+			<li>
+			  <input
+				type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyUp={handleKeyUp}
+				placeholder="Add a new task"
+			  />
+			</li>
+			{tasks.map((task, index) => (
+			  <li key={index} className="task-item">
+				{task}{" "}
+				<button 
+				  onClick={() => removeTask(index)}
+				  className="remove-task-btn"
+				>
+				  x
+				</button>
+			  </li>
+			))}
+		  </ul>
+		  <div>
+			{tasks.length} {tasks.length === 1 ? "task" : "tasks"} left
+		  </div>
 		</div>
+	  </>
 	);
-};
-
-export default Home;
+  };
+  
+  export default Home;
